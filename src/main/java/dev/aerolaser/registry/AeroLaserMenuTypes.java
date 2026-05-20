e6742ee61af5ed2aa53fdd0a9709bcadb9f66e27
DLuis1313 <159087953+DLuis1313@users.noreply.github.com>
2026-05-20 20:09:57 -0300

    Create AeroLaserMenuTypes.java

---
+++ src/main/java/dev/aerolaser/registry/AeroLaserMenuTypes.java
@@ -0,0 +1,20 @@
+package dev.aerolaser.registry;
+
+import dev.aerolaser.AeroLaserMod;
+import dev.aerolaser.block.ShowLaserMenu;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.inventory.MenuType;
+import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class AeroLaserMenuTypes {
+
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(BuiltInRegistries.MENU, AeroLaserMod.MOD_ID);
+
+    public static final DeferredHolder<MenuType<?>, MenuType<ShowLaserMenu>> SHOW_LASER_MENU =
+            MENUS.register("show_laser_menu",
+                    () -> IMenuTypeExtension.create(ShowLaserMenu::new)
+            );
+}
