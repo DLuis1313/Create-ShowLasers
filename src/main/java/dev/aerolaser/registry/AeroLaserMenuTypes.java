86a95b8729e26e3df4685753aab02edf596c7df7
DLuis1313 <159087953+DLuis1313@users.noreply.github.com>
2026-06-04 17:34:42 -0300

    Update AeroLaserMenuTypes.java

--- src/main/java/dev/aerolaser/registry/AeroLaserMenuTypes.java
+++ src/main/java/dev/aerolaser/registry/AeroLaserMenuTypes.java
@@ -1,24 +1,16 @@
 package dev.aerolaser.registry;
-
 import dev.aerolaser.AeroLaserMod;
+import dev.aerolaser.block.GlowLampMenu;
 import dev.aerolaser.block.ShowLaserMenu;
-import dev.aerolaser.block.VeilSpotlightMenu;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
 public class AeroLaserMenuTypes {
-
-    public static final DeferredRegister<MenuType<?>> MENUS =
-            DeferredRegister.create(BuiltInRegistries.MENU, AeroLaserMod.MOD_ID);
-
+    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(BuiltInRegistries.MENU, AeroLaserMod.MOD_ID);
     public static final DeferredHolder<MenuType<?>, MenuType<ShowLaserMenu>> SHOW_LASER_MENU =
-            MENUS.register("show_laser_menu",
-                    () -> IMenuTypeExtension.create(ShowLaserMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<VeilSpotlightMenu>> VEIL_SPOTLIGHT_MENU =
-            MENUS.register("veil_spotlight_menu",
-                    () -> IMenuTypeExtension.create(VeilSpotlightMenu::new));
+            MENUS.register("show_laser_menu", () -> IMenuTypeExtension.create(ShowLaserMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<GlowLampMenu>> GLOW_LAMP_MENU =
+            MENUS.register("glow_lamp_menu", () -> IMenuTypeExtension.create(GlowLampMenu::new));
 }
