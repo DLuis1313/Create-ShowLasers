bd88ec371325591255f515c3bf738371c639349c
DLuis1313 <159087953+DLuis1313@users.noreply.github.com>
2026-06-04 17:37:00 -0300

    Update AeroLaserNetwork.java

--- src/main/java/dev/aerolaser/network/AeroLaserNetwork.java
+++ src/main/java/dev/aerolaser/network/AeroLaserNetwork.java
@@ -1,18 +1,15 @@
 package dev.aerolaser.network;
-
 import dev.aerolaser.AeroLaserMod;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
-
 @EventBusSubscriber(modid = AeroLaserMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class AeroLaserNetwork {
-
     @SubscribeEvent
     public static void registerPackets(RegisterPayloadHandlersEvent event) {
         PayloadRegistrar reg = event.registrar(AeroLaserMod.MOD_ID);
-        reg.playToServer(LaserConfigPacket.TYPE,     LaserConfigPacket.CODEC,     LaserConfigPacket::handle);
-        reg.playToServer(SpotlightConfigPacket.TYPE, SpotlightConfigPacket.CODEC, SpotlightConfigPacket::handle);
+        reg.playToServer(LaserConfigPacket.TYPE,    LaserConfigPacket.CODEC,    LaserConfigPacket::handle);
+        reg.playToServer(GlowLampConfigPacket.TYPE, GlowLampConfigPacket.CODEC, GlowLampConfigPacket::handle);
     }
 }
