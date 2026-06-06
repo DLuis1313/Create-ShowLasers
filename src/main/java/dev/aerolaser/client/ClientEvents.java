5c38e2d9adc88de3536f9bda7b5d53535761e6cd
DLuis1313 <159087953+DLuis1313@users.noreply.github.com>
2026-06-05 22:30:44 -0300

    Update ClientEvents.java

--- src/main/java/dev/aerolaser/client/ClientEvents.java
+++ src/main/java/dev/aerolaser/client/ClientEvents.java
@@ -1,26 +1,29 @@
 package dev.aerolaser.client;
+
 import dev.aerolaser.AeroLaserMod;
 import dev.aerolaser.client.gui.GlowLampScreen;
 import dev.aerolaser.client.gui.ShowLaserScreen;
 import dev.aerolaser.client.renderer.GlowLampRenderer;
 import dev.aerolaser.client.renderer.ShowLaserRenderer;
-import dev.aerolaser.client.renderer.VeilLaserRenderer;
 import dev.aerolaser.registry.AeroLaserBlockEntities;
 import dev.aerolaser.registry.AeroLaserMenuTypes;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+
 @EventBusSubscriber(modid = AeroLaserMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEvents {
+
     @SubscribeEvent
     public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        boolean veil = ModList.get().isLoaded("veil");
-        event.registerBlockEntityRenderer(AeroLaserBlockEntities.SHOW_LASER.get(), veil ? VeilLaserRenderer::new : ShowLaserRenderer::new);
-        event.registerBlockEntityRenderer(AeroLaserBlockEntities.GLOW_LAMP.get(), GlowLampRenderer::new);
+        event.registerBlockEntityRenderer(
+                AeroLaserBlockEntities.SHOW_LASER.get(), ShowLaserRenderer::new);
+        event.registerBlockEntityRenderer(
+                AeroLaserBlockEntities.GLOW_LAMP.get(), GlowLampRenderer::new);
     }
+
     @SubscribeEvent
     public static void onRegisterMenuScreens(RegisterMenuScreensEvent event) {
         event.register(AeroLaserMenuTypes.SHOW_LASER_MENU.get(), ShowLaserScreen::new);
